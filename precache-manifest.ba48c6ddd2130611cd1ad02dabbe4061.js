d2f7e8b2095056cfec8dbaf017ce66b563a5ff7e
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-23 23:48:32 +0600

    Updates

---
+++ precache-manifest.ba48c6ddd2130611cd1ad02dabbe4061.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5693d7f388a71889f97025a683f4de91",
+    "url": "/react-micro-blog/index.html"
+  },
+  {
+    "revision": "18d8f25f0cce833de81a",
+    "url": "/react-micro-blog/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "3b712efa54138d369ea0",
+    "url": "/react-micro-blog/static/css/main.0fd631e7.chunk.css"
+  },
+  {
+    "revision": "18d8f25f0cce833de81a",
+    "url": "/react-micro-blog/static/js/2.18ae4d38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-micro-blog/static/js/2.18ae4d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b712efa54138d369ea0",
+    "url": "/react-micro-blog/static/js/main.d24051e0.chunk.js"
+  },
+  {
+    "revision": "b95bd146f56d2f2278ca",
+    "url": "/react-micro-blog/static/js/runtime-main.bc019aa5.js"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/react-micro-blog/static/media/logo.580ffd26.png"
+  }
+]);
