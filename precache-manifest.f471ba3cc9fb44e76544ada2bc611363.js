874ce476dacd4a566136b40e373adf78242d7402
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-24 01:28:38 +0600

    Updates

---
+++ precache-manifest.f471ba3cc9fb44e76544ada2bc611363.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d52e5dd2965536e7c7bfbf9b391dc3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b5ff146899bf504a88f",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "12460c931e0571dae418",
+    "url": "/static/css/main.0fd631e7.chunk.css"
+  },
+  {
+    "revision": "3b5ff146899bf504a88f",
+    "url": "/static/js/2.865b751e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.865b751e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12460c931e0571dae418",
+    "url": "/static/js/main.ee883b25.chunk.js"
+  },
+  {
+    "revision": "9f28588de26cc1cb6f86",
+    "url": "/static/js/runtime-main.70fc8a2f.js"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/static/media/logo.580ffd26.png"
+  }
+]);
